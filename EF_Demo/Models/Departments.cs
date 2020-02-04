55d284d2d342b13a10047400fc179aa3c7e27f3c
maulik.dave <maulikdave2010@gmail.com>
2020-02-04 17:10:43 +0530

    Add project files.

---
+++ EF_Demo/Models/Departments.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EF_Demo.Models
+{
+    public enum Departments
+    {
+        DOTnet,
+        Mean_Stack,
+        Mern_Stack,
+        ROR,
+        Python
+    }
+}
---
+++ EF_Demo/Models/EmployeeDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EF_Demo.Models
+{
+    public class EmployeeDetails
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int EmpAge { get; set; }
+        public Departments EmpDepartment { get; set; }
+    }
+}
